8c595a2f8348beb1a494ff2f5d3ddb781f4410c3
Venkata Sanjeeva Kuntumalla <kuntumallavenkatasanjeeva2005.com>
2024-09-05 16:23:42 +0530

    added src folder

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const Task = require("./models/tasks"); // Ensure this path is correct
+const Task = require("../models/tasks"); // Ensure this path is correct
 const mongoose = require("mongoose");
 const path = require("path")
 
